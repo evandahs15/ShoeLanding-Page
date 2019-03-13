9aa98df950c16fe5713af555c777a316be281584
Evandah-Steadman <vandahs@outlook.com>
2019-03-14 01:42:46 +1300

    added animation

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,53 @@
-
 // JavaScript
-window.sr = ScrollReveal(); 
+function showNav () {
+  console.log('this is working')
+  var x = document.getElementById('responsive-nav')
+  if (x.className === 'responsive-nav') {
+    x.className += ' unfold'
+  } else {
+    x.className = 'responsive-nav'
+  }
+}
 
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px' 
-// });
+window.sr = ScrollReveal()
+
+sr.reveal('#header img.phone-hero', {
+  delay: 0,
+  duration: 1500,
+  origin: 'bottom',
+  distance: '200px'
+})
+
+sr.reveal('#features div.car', {
+  delay: 200,
+  duration: 300,
+  origin: 'bottom',
+  distance: '100px'
+})
+sr.reveal('#features div.shoe', {
+  delay: 400,
+  duration: 300,
+  origin: 'bottom',
+  distance: '100px'
+})
+
+sr.reveal('#features div.phone', {
+  delay: 600,
+  duration: 300,
+  origin: 'bottom',
+  distance: '100px'
+})
+
+sr.reveal('#discount .offer', {
+  delay: 600,
+  duration: 300,
+  origin: 'right',
+  distance: '200px'
+})
+
+sr.reveal('#discount picture', {
+  delay: 600,
+  duration: 300,
+  origin: 'left',
+  distance: '200px'
+})
